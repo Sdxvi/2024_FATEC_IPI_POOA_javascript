e0787ec8e7d657c00c12220c40f3361688570c7d
Vinicius Giacomelli <vinicius.giacomelli@fatec.sp.gov.br>
2024-03-06 18:18:35 -0300

    js(sync-async): fim do codigo 3.3.1
    - vimos conceitos sobre promises
    - implementamos 1+2+...+n da maneira inocente
    - vimos exemplo básico com resolve e reject fazendo soma
    - retomar fazendo o calculo de pa rapidinho

--- index.js
+++ index.js
@@ -1,26 +1,57 @@
-const fs = require('fs')
-const abrirArquivo = (nomeArquivo) => {
-    const exibirConteudo = (erro, conteudo) => {
-            if(erro){
-                console.log(`Erro: ${erro}`)
-            }
-            else{
-                console.log(conteudo.toString())
-                const resultado = +conteudo.toString() * 10 //+conteudo.toString, transforma a string em int
-                const finalizar = (erro) => {
-                    if(!erro) {
-                        console.log('Conteúdo escrito com sucesso')
-                    }
-                    else{
-                        console.log('Escrita Falhou')
-                    }
-                }
-                fs.writeFile('resultado.txt', resultado.toString(), finalizar)
-            }
-    }
-    fs.readFile(nomeArquivo, exibirConteudo)
+
+// 1 + 2 + 3 + . . . + n -2 + n - 1 + n
+// const calculoDemorado = (n) => {
+//     let p = new Promise((resolve, reject) => {
+//         let res = 0
+//         for (let i = 1; i <= n; i++) res += i
+//         resolve(res)
+//     })
+//     return p
+// }
+
+// const aux = calculoDemorado(3)
+// aux.then((resultado) => {console.log(resultado)})
+// console.log(aux)
+
+
+const soma = (a, b) => {
+    let p = new Promise((resolve, reject) => {
+        //se a e b forem positivos
+        //chamar resolve passando a + b como parâmetro
+        // caso contrário
+        // chamar reject passando texto "não use negativos" como parâmetro
+        if(a >= 0 && b >= 0) resolve(a+b)
+        else reject('não use negativos')
+    })
+    return p
 }
-abrirArquivo('arquivo.txt')
+
+soma(-2, -7).then(res => console.log(`Resultado: ${res}`)).catch ((erro) => console.log(erro))
+//descobrir como faz o catch
+
+// const fs = require('fs')
+// const abrirArquivo = (nomeArquivo) => {
+//     const exibirConteudo = (erro, conteudo) => {
+//             if(erro){
+//                 console.log(`Erro: ${erro}`)
+//             }
+//             else{
+//                 console.log(conteudo.toString())
+//                 const resultado = +conteudo.toString() * 10 //+conteudo.toString, transforma a string em int
+//                 const finalizar = (erro) => {
+//                     if(!erro) {
+//                         console.log('Conteúdo escrito com sucesso')
+//                     }
+//                     else{
+//                         console.log('Escrita Falhou')
+//                     }
+//                 }
+//                 fs.writeFile('resultado.txt', resultado.toString(), finalizar)
+//             }
+//     }
+//     fs.readFile(nomeArquivo, exibirConteudo)
+// }
+// abrirArquivo('arquivo.txt')
 
 
 // function demorada (){
